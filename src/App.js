41fa979673d1d09e85dda324ac76064045288bd4
raihan2bd <raihan2bd.official@gmail.com>
2023-07-11 18:21:50 +0600

    Add the necessary routes to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-const App = () => (
-  <main>
-    <h1>Hello world</h1>
-  </main>
-);
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/greeting" element={<Greeting />} />
+    </Routes>
+  );
+}
 
 export default App;
